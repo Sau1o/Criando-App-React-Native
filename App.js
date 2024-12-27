dad5b85afd6c20e1dd7eaccd904cb171fdd7e2c7
Saulo Coelho <coelho.saulo@gmail.com>
2024-12-26 21:12:18 -0300

    feat: added font Montserrat

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
+import { 
+  useFonts, 
+  Montserrat_400Regular, 
+  Montserrat_700Bold 
+} from '@expo-google-fonts/montserrat';
 
 const App = () => {
+  const [fontCarregada] = useFonts({
+    "MontserratRegular": Montserrat_400Regular,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if(!fontCarregada) {
+    return <View />
+  }
+
   return (
     <SafeAreaView>
       <StatusBar />
